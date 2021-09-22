cf5f10498d3ae20aa963d7c4075bfac788bc134f
EduardoZirbell <88445992+EduardoZirbell@users.noreply.github.com>
2021-09-22 14:41:59 -0300

    Update urls(Eduardo)

--- assis_tech/urls.py
+++ assis_tech/urls.py
@@ -5,8 +5,12 @@ urlpatterns = [
   path('', views.index, name='index'),
   path('report/', views.report, name='report'),
   path('register/', views.registerPage, name="register"),
-	path('login/', views.loginPage, name="login"),
-	path('logout/', views.logoutUser, name="logout"),
-
+  path('login/', views.loginPage, name="login"),
+  path('logout/', views.logoutUser, name="logout"),
+  path('dashboard/', views.dashboard, name='dashboard'),
+  path('dashboard/detalhe/<int:pk>/', views.detail, name='detail'),
+  path('dashboard/editar/<int:pk>/', views.edit, name='edit'),
+  path('dashboard/atualizar/<int:pk>/', views.update, name='update'),
+  path('dashboard/deletar/<int:pk>/', views.delete, name='delete'),
 ]
 
