078a6818f8672c29e9aab565bd3ddbcc4815564b
EduardoZirbell <88445992+EduardoZirbell@users.noreply.github.com>
2021-09-22 14:36:31 -0300

    Update models(Eduardo).

--- assis_tech/models.py
+++ assis_tech/models.py
@@ -80,18 +80,32 @@ class Account(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
 
+# class categoria_relato(models.Model):
+#     nome = models.CharField(max_length=100)
 
+# class relato(models.Model):
+#     nome				= models.CharField(max_length=100)
+#     cpf_user 			= models.CharField(max_length=30, unique=True)
+#     telefone 			= models.CharField(max_length=12)
+#     email 				= models.CharField(max_length=255, blank=True)
+#     data_criacao 		= models.DateTimeField(default=timezone.now)
+#     descricao 			= models.TextField(blank=True)
 
-
-
-
-class categoria_relato(models.Model):
+class Categoria(models.Model):
     nome = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nome
 
-class relato(models.Model):
-    nome				= models.CharField(max_length=100)
-    cpf_user 			= models.CharField(max_length=30, unique=True)
-    telefone 			= models.CharField(max_length=12)
-    email 				= models.CharField(max_length=255, blank=True)
-    data_criacao 		= models.DateTimeField(default=timezone.now)
-    descricao 			= models.TextField(blank=True)
+class Relato(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    local = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
