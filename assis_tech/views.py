099dfc9a5a7231ea66f97d46f6eef6357f79c8f9
Kaue Lunelli <lunellikaue@gmail.com>
2021-09-26 22:26:49 -0300

    Alterar foto de perfil

--- assis_tech/views.py
+++ assis_tech/views.py
@@ -7,7 +7,14 @@ from django.contrib.auth.forms import UserCreationForm
 from assis_tech.form import CreateUserForm, AccountAuthenticationForm, AccountUpdateForm
 from django.contrib.auth import authenticate, login, logout
 import folium
-
+import cv2
+import json
+import base64
+import requests
+import os
+from django.core.files.storage import FileSystemStorage
+from django.core.files.storage import default_storage
+from django.core import files
 from .form import RelatoForm
 from .models import Relato
 from django.core.paginator import Paginator
@@ -19,7 +26,8 @@ from django.shortcuts import render
 from django.conf import settings
 from .models import Account
 
-# Create your views here.
+
+TEMP_PROFILE_IMAGE_NAME = "temp_profile_image.png"
 
 
 def index(request):
@@ -74,7 +82,7 @@ def loginPage(request):
         messages.add_message(request, messages.ERROR,
                              'Email ou Senha Inválido.')
 
-        return redirect('index')
+        return redirect('login')
         messages.add_message(request, messages.ERROR,
                              'Email ou Senha Inválido.')
 
@@ -91,14 +99,6 @@ def logoutUser(request):
 
 
 def account_view(request, *args, **kwargs):
-    """
-    - Logic here is kind of tricky
-            is_self (boolean)
-                    is_friend (boolean)
-                            -1: NO_REQUEST_SENT
-                            0: THEM_SENT_TO_YOU
-                            1: YOU_SENT_TO_THEM
-    """
     context = {}
     user_id = kwargs.get("user_id")
     try:
@@ -122,7 +122,7 @@ def account_view(request, *args, **kwargs):
 
         # Set the template variables to the values
         context['is_self'] = is_self
-        return render(request, "pages/account.html", context)
+        return render(request, "pages/dashboard.html", context)
 
 
 def edit_account_view(request, *args, **kwargs):
@@ -137,9 +137,9 @@ def edit_account_view(request, *args, **kwargs):
         form = AccountUpdateForm(
             request.POST, request.FILES, instance=request.user)
         if form.is_valid():
+            account.profile_image.delete()
             form.save()
-            new_username = form.cleaned_data['username']
-            return redirect("account:view", user_id=account.pk)
+            return redirect("view", user_id=account.pk)
         else:
             form = AccountUpdateForm(request.POST, instance=request.user,
                                      initial={
@@ -164,6 +164,76 @@ def edit_account_view(request, *args, **kwargs):
         context['form'] = form
     context['DATA_UPLOAD_MAX_MEMORY_SIZE'] = settings.DATA_UPLOAD_MAX_MEMORY_SIZE
     return render(request, "pages/edit_account.html", context)
+
+
+
+def save_temp_profile_image_from_base64String(imageString, user):
+	INCORRECT_PADDING_EXCEPTION = "Incorrect padding"
+	try:
+		if not os.path.exists(settings.TEMP):
+			os.mkdir(settings.TEMP)
+		if not os.path.exists(settings.TEMP + "/" + str(user.pk)):
+			os.mkdir(settings.TEMP + "/" + str(user.pk))
+		url = os.path.join(settings.TEMP + "/" + str(user.pk),TEMP_PROFILE_IMAGE_NAME)
+		storage = FileSystemStorage(location=url)
+		image = base64.b64decode(imageString)
+		with storage.open('', 'wb+') as destination:
+			destination.write(image)
+			destination.close()
+		return url
+	except Exception as e:
+		print("exception: " + str(e))
+		# workaround for an issue I found
+		if str(e) == INCORRECT_PADDING_EXCEPTION:
+			imageString += "=" * ((4 - len(imageString) % 4) % 4)
+			return save_temp_profile_image_from_base64String(imageString, user)
+	return None
+
+
+def crop_image(request, *args, **kwargs):
+	payload = {}
+	user = request.user
+	if request.POST and user.is_authenticated:
+		try:
+			imageString = request.POST.get("image")
+			url = save_temp_profile_image_from_base64String(imageString, user)
+			img = cv2.imread(url)
+
+			cropX = int(float(str(request.POST.get("cropX"))))
+			cropY = int(float(str(request.POST.get("cropY"))))
+			cropWidth = int(float(str(request.POST.get("cropWidth"))))
+			cropHeight = int(float(str(request.POST.get("cropHeight"))))
+			if cropX < 0:
+				cropX = 0
+			if cropY < 0: # There is a bug with cropperjs. y can be negative.
+				cropY = 0
+			crop_img = img[cropY:cropY+cropHeight, cropX:cropX+cropWidth]
+
+			cv2.imwrite(url, crop_img)
+
+			# delete the old image
+			user.profile_image.delete()
+
+			# Save the cropped image to user model
+			user.profile_image.save("profile_image.png", files.File(open(url, 'rb')))
+			user.save()
+
+			payload['result'] = "success"
+			payload['cropped_profile_image'] = user.profile_image.url
+
+			# delete temp file
+			os.remove(url)	
+		except Exception as e:
+			print("exception: " + str(e))
+			payload['result'] = "error"
+			payload['exception'] = str(e)
+	return HttpResponse(json.dumps(payload), content_type="application/json")
+
+
+
+
+
+
 
 
 def report(request):
