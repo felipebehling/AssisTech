715c1e346622e5c3a8551383658296c36912846b
Welinton Aires de Oliveira <weliton173@gmail.com>
2021-09-29 17:23:47 -0300

    feature/edit,dashboard e detalhe arrumados

--- assis_tech/filters.py
+++ assis_tech/filters.py
@@ -1,8 +1,11 @@
 import django_filters
 from .models import Relato
+from django_filters import CharFilter
 
 
 class RelatoFilter(django_filters.FilterSet):
+    nome = CharFilter(field_name='nome', lookup_expr='icontains')
+    cpf = CharFilter(field_name="cpf", lookup_expr='icontains')
     class Meta:
         model = Relato
         fields = ['nome', 'cpf', 'categoria']
